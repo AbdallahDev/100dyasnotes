bba3b4dd8852494cb79ede6f5b3353f3c40c3ca6
abdallah.sarayrah <abdallah.dev85@gmail.com>
2022-05-01 16:14:00 +0300

    note

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # in python, it doesn't matter if I use single quote '' or double quote ""
+#
 # to combine two strings in python i should use the + character, it's called strings concatenation
 # ex:
 print("Hello" + " " + "World")
